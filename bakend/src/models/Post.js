a4aabe1ff882909235c56c402454a26e24dee90c
Johan Navarro <21sebas12@gmail.com>
2020-01-27 20:08:43 -0500

    add config postgress

---
+++ bakend/src/models/Post.js
@@ -0,0 +1,39 @@
+export default (sequelize, {BOOLEAN, STRING, TEXT, UUID, UUIDV4}) =>{
+    const Post = sequelize.define('Post', {
+        id:{
+            primaryKey:true,
+            allowNull:false,
+            type: UUID,
+            defaultValue:UUIDV4()
+        },
+        title:{
+            type:STRING,
+            allowNull:false
+        },
+        slug:{
+            type:STRING,
+            allowNull:false
+        },
+        readingTime:{
+            type:STRING,
+            allowNull:false,
+            defaultValue:'3 min'
+        },
+        body:{
+            type:TEXT, 
+            allowNull:false
+        },
+        language:{ // Para que sea de diferentes idiomas 
+            type:STRING,
+            allowNull:false,
+            defaultValue:'es'
+        },
+        image:{
+            type:STRING
+        },
+        published:{//Si esta publicado
+            type:BOOLEAN,
+            defaultValue:false
+        }
+    });
+}
